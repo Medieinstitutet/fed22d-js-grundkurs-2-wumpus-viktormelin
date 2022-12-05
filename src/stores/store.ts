6b5635b8df50c4a25422d80fadc4a653a890c583
dough <viktor.melin@me.com>
2022-12-05 17:24:23 +0100

    🌐 added locales for danger variants

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,7 +1,13 @@
-import { GlobalVariables, Position } from '@/typings/types';
+import { GlobalVariables, Locales, Position } from '@/typings/types';
 
 export const boardElement = document.querySelector('.board') as HTMLElement;
 export const leaderboardElement = document.querySelector('.leaderboard') as HTMLElement;
+export const informationElement = document.querySelector('.information') as HTMLElement;
 export const startGameButton = document.querySelector('#startGameButton') as HTMLButtonElement;
 export const keyboardKeys = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown', 'Enter'];
 export const gameBoard: Position[] = [];
+export const locales: Locales = {
+  bat: 'Du hör nånting fladdra',
+  hole: 'Du känner ett vinddrag',
+  wumpus: 'Du känner en konstig lukt',
+};
