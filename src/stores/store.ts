bef694251375d62717745476b5f588745e6111f2
dough <viktor.melin@me.com>
2022-12-06 13:44:27 +0100

    ♻️ cleanup

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,16 +1,25 @@
 import { Locales, Position, Scores } from '@/typings/types';
 
 export const boardElement = document.querySelector('.board') as HTMLElement;
+
+export const modalElement = document.querySelector('.modal') as HTMLElement;
+
 export const leaderboardElement = document.querySelector('.leaderboard') as HTMLElement;
+
 export const informationElement = document.querySelector('.information') as HTMLElement;
+
 export const startGameButton = document.querySelector('#startGameButton') as HTMLButtonElement;
+
 export const keyboardKeys = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown', 'Enter'];
+
 export const gameBoard: Position[] = [];
+
 export const locales: Locales = {
   bat: 'Du hör nånting fladdra',
   hole: 'Du känner ett vinddrag',
   wumpus: 'Du känner en konstig lukt',
 };
+
 export const scores: Scores = {
   step: 1,
   bat: 2,
