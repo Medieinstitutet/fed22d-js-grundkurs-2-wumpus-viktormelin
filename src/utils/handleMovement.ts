eb59128977f504751f3e475a004a87c9bcd97ca1
dough <viktor.melin@me.com>
2022-12-08 00:54:58 +0100

    ✨ arrow mode

--- src/utils/handleMovement.ts
+++ src/utils/handleMovement.ts
@@ -1,8 +1,15 @@
 import { gameBoard, keyboardKeys } from '@/stores/store';
 import Global from '@/stores/variables';
 import updatePlayerPosition from '@/utils/updatePlayerPosition';
+import shootArrow from './shootArrow';
 
-const HandleMovement = (e: KeyboardEvent) => {
+const activateArrowMode = () => {
+  if (!Global.isArrowMode) {
+    Global.isArrowMode = true;
+  }
+};
+
+const handleMovement = (e: KeyboardEvent) => {
   if (keyboardKeys.includes(e.key) && Global.isGameStarted) {
     const oldCoords = gameBoard[Global.currentPosition].coord;
 
@@ -10,64 +17,91 @@ const HandleMovement = (e: KeyboardEvent) => {
 
     switch (e.key) {
       case 'ArrowLeft':
-        if (oldCoords[1] === 0) {
-          Global.currentPosition = gameBoard.findIndex((item) => item.coord[1] === 4 && item.coord[0] === oldCoords[0]);
-          // gameBoard[currentPosition].coord[1] = 4;
-        } else {
-          Global.currentPosition = gameBoard.findIndex(
-            (item) => item.coord[1] === oldCoords[1] - 1 && item.coord[0] === oldCoords[0]
-          );
-          // gameBoard[currentPosition].coord[1] -= 1;
-        }
+        if (!Global.isArrowMode) {
+          if (oldCoords[1] === 0) {
+            Global.currentPosition = gameBoard.findIndex(
+              (item) => item.coord[1] === 4 && item.coord[0] === oldCoords[0]
+            );
+            // gameBoard[currentPosition].coord[1] = 4;
+          } else {
+            Global.currentPosition = gameBoard.findIndex(
+              (item) => item.coord[1] === oldCoords[1] - 1 && item.coord[0] === oldCoords[0]
+            );
+            // gameBoard[currentPosition].coord[1] -= 1;
+          }
 
-        // gameBoard[currentPosition].id = gameBoard[currentPosition].coord.toString().replace(',', '');
-        updatePlayerPosition('left');
+          // gameBoard[currentPosition].id = gameBoard[currentPosition].coord.toString().replace(',', '');
+          updatePlayerPosition('left');
+        } else {
+          shootArrow('left');
+        }
 
         break;
       case 'ArrowRight':
-        if (oldCoords[1] === 4) {
-          Global.currentPosition = gameBoard.findIndex((item) => item.coord[1] === 0 && item.coord[0] === oldCoords[0]);
-          // currentPosition.coord[1] = 0;
-        } else {
-          Global.currentPosition = gameBoard.findIndex(
-            (item) => item.coord[1] === oldCoords[1] + 1 && item.coord[0] === oldCoords[0]
-          );
-          // currentPosition.coord[1] += 1;
-        }
+        if (!Global.isArrowMode) {
+          if (oldCoords[1] === 4) {
+            Global.currentPosition = gameBoard.findIndex(
+              (item) => item.coord[1] === 0 && item.coord[0] === oldCoords[0]
+            );
+            // currentPosition.coord[1] = 0;
+          } else {
+            Global.currentPosition = gameBoard.findIndex(
+              (item) => item.coord[1] === oldCoords[1] + 1 && item.coord[0] === oldCoords[0]
+            );
+            // currentPosition.coord[1] += 1;
+          }
 
-        // currentPosition.id = currentPosition.coord.toString().replace(',', '');
-        updatePlayerPosition('right');
+          // currentPosition.id = currentPosition.coord.toString().replace(',', '');
+          updatePlayerPosition('right');
+        } else {
+          shootArrow('right');
+        }
 
         break;
       case 'ArrowUp':
-        if (oldCoords[0] === 0) {
-          Global.currentPosition = gameBoard.findIndex((item) => item.coord[1] === oldCoords[1] && item.coord[0] === 3);
-          // currentPosition.coord[0] = 3;
-        } else {
-          Global.currentPosition = gameBoard.findIndex(
-            (item) => item.coord[1] === oldCoords[1] && item.coord[0] === oldCoords[0] - 1
-          );
-          // currentPosition.coord[0] -= 1;
-        }
+        if (!Global.isArrowMode) {
+          if (oldCoords[0] === 0) {
+            Global.currentPosition = gameBoard.findIndex(
+              (item) => item.coord[1] === oldCoords[1] && item.coord[0] === 3
+            );
+            // currentPosition.coord[0] = 3;
+          } else {
+            Global.currentPosition = gameBoard.findIndex(
+              (item) => item.coord[1] === oldCoords[1] && item.coord[0] === oldCoords[0] - 1
+            );
+            // currentPosition.coord[0] -= 1;
+          }
 
-        // currentPosition.id = currentPosition.coord.toString().replace(',', '');
-        updatePlayerPosition('up');
+          // currentPosition.id = currentPosition.coord.toString().replace(',', '');
+          updatePlayerPosition('up');
+        } else {
+          shootArrow('up');
+        }
 
         break;
       case 'ArrowDown':
-        if (oldCoords[0] === 3) {
-          Global.currentPosition = gameBoard.findIndex((item) => item.coord[1] === oldCoords[1] && item.coord[0] === 0);
-          // currentPosition.coord[0] = 0;
+        if (!Global.isArrowMode) {
+          if (oldCoords[0] === 3) {
+            Global.currentPosition = gameBoard.findIndex(
+              (item) => item.coord[1] === oldCoords[1] && item.coord[0] === 0
+            );
+            // currentPosition.coord[0] = 0;
+          } else {
+            Global.currentPosition = gameBoard.findIndex(
+              (item) => item.coord[1] === oldCoords[1] && item.coord[0] === oldCoords[0] + 1
+            );
+            // currentPosition.coord[0] += 1;
+          }
+
+          // currentPosition.id = currentPosition.coord.toString().replace(',', '');
+          updatePlayerPosition('down');
         } else {
-          Global.currentPosition = gameBoard.findIndex(
-            (item) => item.coord[1] === oldCoords[1] && item.coord[0] === oldCoords[0] + 1
-          );
-          // currentPosition.coord[0] += 1;
+          shootArrow('down');
         }
 
-        // currentPosition.id = currentPosition.coord.toString().replace(',', '');
-        updatePlayerPosition('down');
-
+        break;
+      case 'Enter':
+        activateArrowMode();
         break;
       default:
         break;
@@ -75,4 +109,4 @@ const HandleMovement = (e: KeyboardEvent) => {
   }
 };
 
-export default HandleMovement;
+export default handleMovement;
