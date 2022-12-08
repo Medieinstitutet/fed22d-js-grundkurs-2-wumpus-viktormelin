4d0d4f02f00d0f4b5f785ce9d646b0f90ff2b564
dough <viktor.melin@me.com>
2022-12-08 17:14:35 +0100

    ✨ shoot arrows

--- src/utils/shootArrow.ts
+++ src/utils/shootArrow.ts
@@ -1,31 +1,81 @@
 import { gameBoard } from '@/stores/store';
 import Global from '@/stores/variables';
+import endGame from './endGame';
 
 const shootArrow = (direction: string) => {
   const { coord } = gameBoard[Global.currentPosition];
-  const closestCoords = [];
   const coordsToFind = [];
-  const travelTiles = 3;
 
   switch (direction) {
     case 'left':
-      for (let i = 1; i < 4; i += 1) {
-        if (coord[1] - i >= 0) {
-          coordsToFind.push(gameBoard.find((item) => item.coord[0] === coord[0] && item.coord[1] === i));
+      for (let i = 0; i < 3; i += 1) {
+        const closestWCoord = coord[1] - 1 >= 0 ? coord[1] - 1 : 4;
+        if (closestWCoord - i >= 0) {
+          coordsToFind.push(
+            gameBoard.find((item) => item.coord[0] === coord[0] && item.coord[1] === closestWCoord - i)
+          );
         } else {
-          coordsToFind.push(gameBoard.find((item) => item.coord[0] === coord[0] && item.coord[1] === i));
+          const movement = closestWCoord - (i - 1) - 4 * -1;
+          coordsToFind.push(gameBoard.find((item) => item.coord[0] === coord[0] && item.coord[1] === movement));
         }
       }
-
       break;
     case 'right':
+      for (let i = 0; i < 3; i += 1) {
+        const closestECoord = coord[1] + 1 <= 4 ? coord[1] + 1 : 0;
+
+        if (closestECoord + i <= 4) {
+          coordsToFind.push(
+            gameBoard.find((item) => item.coord[0] === coord[0] && item.coord[1] === closestECoord + i)
+          );
+        } else {
+          let movement = closestECoord - (i + 1);
+
+          movement = movement === 0 ? movement : movement - 1;
+          movement = movement >= 0 ? movement : movement * -1;
+
+          coordsToFind.push(gameBoard.find((item) => item.coord[0] === coord[0] && item.coord[1] === movement));
+        }
+      }
       break;
     case 'up':
+      for (let i = 0; i < 3; i += 1) {
+        const closestNCoord = coord[0] - 1 >= 0 ? coord[0] - 1 : 3;
+
+        if (closestNCoord - i >= 0) {
+          coordsToFind.push(
+            gameBoard.find((item) => item.coord[0] === closestNCoord - i && item.coord[1] === coord[1])
+          );
+        } else {
+          const movement = closestNCoord - (i - 1) - 3 * -1;
+          coordsToFind.push(gameBoard.find((item) => item.coord[0] === movement && item.coord[1] === coord[1]));
+        }
+      }
       break;
     case 'down':
+      for (let i = 0; i < 3; i += 1) {
+        const closestSCoord = coord[0] + 1 <= 3 ? coord[0] + 1 : 0;
+
+        if (closestSCoord + i <= 3) {
+          coordsToFind.push(
+            gameBoard.find((item) => item.coord[0] === closestSCoord + i && item.coord[1] === coord[1])
+          );
+        } else {
+          let movement = closestSCoord - (i + 1);
+
+          movement = movement === -1 ? 0 : movement;
+          coordsToFind.push(gameBoard.find((item) => item.coord[0] === movement && item.coord[1] === coord[1]));
+        }
+      }
       break;
     default:
       break;
+  }
+
+  for (const tile of coordsToFind) {
+    if (tile?.danger === 'wumpus') {
+      endGame('win');
+    }
   }
 };
 
