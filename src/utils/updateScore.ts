605b345a36c14130254bba20bbde40ac47e70cb7
dough <viktor.melin@me.com>
2022-12-09 18:32:31 +0100

    🐛 fix for score update after some changes

--- src/utils/updateScore.ts
+++ src/utils/updateScore.ts
@@ -3,8 +3,11 @@ import Global from '@/stores/variables';
 
 const updateScore = (score: number) => {
   Global.score += score;
-  const scoreElement = informationElement.querySelector('.information__score p') as HTMLElement;
-  scoreElement.innerHTML = `Poäng: ${Global.score}`;
+
+  const information = informationElement.querySelector('.information__score') as HTMLElement;
+  const scoreSpanElement = information.querySelector('.information__score_points span') as HTMLSpanElement;
+
+  scoreSpanElement.innerText = String(Global.score);
 };
 
 export default updateScore;
